2dfaca659b9b0ab065bc4d8852f7fd3c7464170a
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-02-13 14:08:47 +0100

    Add solution & project

---
+++ Global.Usings/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Global.Usings;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    
+    public void SomeEmptyMethod()
+    {
+    }
+}
---
+++ Global.Usings/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Global.Usings;
+
+string json = """
+              {
+                "name": "John",
+                "age": 30
+              }
+              """;
+
+var person = JsonSerializer.Deserialize<Person>(json,
+    new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    });
+
+Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
